af54be26b2618f5f5a81cd847619fe7d83708ff8
eliyahus <eliyahu.shvalb@gmail.com>
2022-07-03 18:55:07 +0300

    temporarily comment method which causes our scan flow to fail

--- src/main/java/com/sanity/scan/controller/UserController.java
+++ src/main/java/com/sanity/scan/controller/UserController.java
@@ -64,11 +64,12 @@ public class UserController {
         return "LioR";
     }
 
-    @RequestMapping("/user/update/name")
-    public void updateUserName(@RequestParam(value = "name", required = true) String name,
-            @RequestParam(value = "id", required = true) String id) {
-        userService.updateUserName(id, name);
-    }
+    // our scan flow fails if this method is not commented, because of the SAST bug in meta queries (AS-2065)
+//    @RequestMapping("/user/update/name")
+//    public void updateUserName(@RequestParam(value = "name", required = true) String name,
+//            @RequestParam(value = "id", required = true) String id) {
+//        userService.updateUserName(id, name);
+//    }
 
 
 }
